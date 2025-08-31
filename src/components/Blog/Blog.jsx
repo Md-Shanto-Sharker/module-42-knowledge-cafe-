eb3256714358ee24a4e1524fc48d672e84d48a6f
SHANTO <rabbymd55555@gmail.com>
2025-08-31 09:04:36 +0600

    added knowledge cafe

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React from "react";
+import { FaBookmark } from "react-icons/fa";
 
-const Blog = ({blog}) => {
-    
-    return (
-        <div>
-           <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img className='h-32 w-full'
-      src={blog.cover}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{blog.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Mark as read</button>
-    </div>
-  </div>
-</div>
+const Blog = ({ blog }) => {
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img className="h-56 w-full" src={blog.cover} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <div className="author flex justify-around items-center">
+            <h2>{blog.author}</h2>
+            <img className="w-16" src={blog.author_img} alt="" />
+            <FaBookmark size={25}/>
+          </div>
+          <h2 className="card-title">{blog.title}</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div className="flex justify-around">
+            {blog.hashtags.map((has) => (
+              <h1 has={has}>{has}</h1>
+            ))}
+          </div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Mark as read</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
